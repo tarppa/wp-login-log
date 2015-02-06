7e5544961c01252973511c05f7ffb08551d14f0f
tarppa <tarppa@users.noreply.github.com>
2015-02-06 14:15:21 +0200

    Basic filehandling done

--- wp-login-logging.php
+++ wp-login-logging.php
@@ -1,12 +1,15 @@
 <?php
-# Wordpress plugin for writing login attempts to a file
+	# Wordpress plugin for writing login attempts to a file
 
-function log_write() {
-    // your code
-}
-add_action('wp_signon', 'log_write');
+	function log_write() {
+	    
+	$content = "timestamp-username-failed/successful\n";
 
-
-
+	$handle = fopen('login.log','a+');
+	fwrite($handle,$content)
+	fclose($handle);
+	}
+	
+	add_action('wp_signon', 'log_write');
 
 ?>
