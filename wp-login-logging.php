25a17a98c382492a3764d732a627e234a00a03e9
tarppa <tarppa@users.noreply.github.com>
2015-02-08 19:49:14 +0200

    more cleaning of the code, functionality stays the same

--- wp-login-logging.php
+++ wp-login-logging.php
@@ -3,43 +3,38 @@
  *	Plugin Name: Login logging
  * 	Description: Write all login attempts to a logfile
  *	Author: https://github.com/tarppa/
- * 	Version: 0
- *  License: NO WARRANTY. DO WHATEVER YOU LIKE WITH THIS
+ * 	Version: 1.0
+ *  License: GPL2
  */
-	function write_to_log() {
+	function write_to_log() {	
 		
-		#get login data    
 		$username  = $_POST['log'];
 		$attempt   = '';
 		$password  = $_POST['pwd'];
-		$timestamp = date("Y-m-d H:i:s");
+		$timestamp = date( "Y-m-d H:i:s" );
 		
-		if(empty($username)){
-			
+		if ( empty( $username ) ){
 			$username = 'EMPTY_USERNAME';
 		}
 
-		$user = wp_authenticate_username_password(null, $username, $password);
+		$user = wp_authenticate_username_password( null, $username, $password );
 		
-		if(is_wp_error($user)) {
-		 
-		 $attempt = 'FAILURE'; 
+		if ( is_wp_error( $user ) ) {
+			$attempt = 'FAILURE'; 
 		
+		} else {
+			$attempt = 'SUCCESS';	
 		}
-		else {
-			
-		$attempt = 'SUCCESS';	
-		}
-		$content = array($timestamp, $username, $attempt,"\n"); 
-		$content = implode(" ", $content);
+		
+		$content = array( $timestamp, $username, $attempt,"\n" ); 
+		$content = implode( " ", $content );
 		
 		# write to file
-		$file_handle = fopen('/home/tari/data/log/login.log', 'a+');
-		fwrite($file_handle, $content);
-		fclose($file_handle);
+		$file_handle = fopen( '/home/tari/data/log/login.log', 'a+' );
+		fwrite( $file_handle, $content );
+		fclose( $file_handle );
 	}
 	
-	#wp_login is a deprecated function, but in this case it's to be used
-	add_action('wp_authenticate', 'write_to_log');
+	add_action( 'wp_authenticate', 'write_to_log' );
 
 ?>
